184cfc24a0ef473d6f0dca9afdd7648db19b7e65
own-idea <m_tfn@126.com>
2014-11-07 21:28:44 +0800

    add insert sort code

--- IntroductionToAlgorithms/src/main/java/introduction/to/algorithms/chapter1/InsertSort.java
+++ IntroductionToAlgorithms/src/main/java/introduction/to/algorithms/chapter1/InsertSort.java
@@ -2,15 +2,13 @@ package introduction.to.algorithms.chapter1;
 
 public class InsertSort {
 
-	public static int[] sort(int[] a) {
-		int[] b = new int[a.length];
-		System.arraycopy(a, 0, b, 0, a.length);
-		for (int j = 1; j < b.length; j++) {
+	public static void sort(int[] a) {
+		for (int j = 1; j < a.length; j++) {
 			int key = a[j];
-			for (int i = j - 1; i >= 0 && b[i] > key; i--) {
-				int temp = b[i];
-				b[i] = key;
-				b[i + 1] = temp;
+			for (int i = j - 1; i >= 0 && a[i] > key; i--) {
+				int temp = a[i];
+				a[i] = key;
+				a[i + 1] = temp;
 			}
 			// 第二种实现方式
 	/*		for (int i = j; i >0; i--) {
@@ -21,13 +19,10 @@ public class InsertSort {
 				}
 			}*/
 		}
-		return b;
 	}
 	
-	// 醋，酒瓶子
-	public void swap(int a, int b) {
-		int temp = a;
-		a = b;
-		b = temp;
+	public static <T extends Comparable<T>> void sort(T[] array) {
+
 	}
+	
 }
--- IntroductionToAlgorithms/src/test/java/introduction/to/algorithms/chapter1/TestInsertSort.java
+++ IntroductionToAlgorithms/src/test/java/introduction/to/algorithms/chapter1/TestInsertSort.java
@@ -8,36 +8,36 @@ public class TestInsertSort {
 	@Test
 	public void 测试只有1个元素() {
 		int[] a = { 8 };
-		int[] result = InsertSort.sort(a);
-		assertArrayEquals(new int[] { 8 }, result);
+		InsertSort.sort(a);
+		assertArrayEquals(new int[] { 8 }, a);
 	}
 
 	@Test
 	public void 测试2个元素且已排序() {
 		int[] a = { 6, 8 };
-		int[] result = InsertSort.sort(a);
-		assertArrayEquals(new int[] { 6, 8 }, result);
+		InsertSort.sort(a);
+		assertArrayEquals(new int[] { 6, 8 }, a);
 	}
 
 	@Test
 	public void 测试2个元素未排序() {
 		int[] a = { 8, 6 };
-		int[] result = InsertSort.sort(a);
-		assertArrayEquals(new int[] { 6, 8 }, result);
+		InsertSort.sort(a);
+		assertArrayEquals(new int[] { 6, 8 }, a);
 	}
 
 	@Test
 	public void 测试多个元素() {
 		int[] a = { 5, 2, 4, 6, 1, 3 };
-		int[] result = InsertSort.sort(a);
-		assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, result);
+		InsertSort.sort(a);
+		assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, a);
 	}
 
 	@Test
 	public void 测试含重复元素() {
 		int[] a = { 5, 2, 5, 6, 1, 6, 2 };
-		int[] result = InsertSort.sort(a);
-		assertArrayEquals(new int[] { 1, 2, 2, 5, 5, 6, 6 }, result);
+		InsertSort.sort(a);
+		assertArrayEquals(new int[] { 1, 2, 2, 5, 5, 6, 6 }, a);
 	}
 
 }
